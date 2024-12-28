91296d8162ea72fb92394308959cafa146f525e4
reef <public@ralph.tech>
2024-12-28 12:15:01 +0100

    implemented stdError for registry and added retrieval method

--- workspace/schema_registry/src/lib.rs
+++ workspace/schema_registry/src/lib.rs
@@ -1,16 +1,7 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, error::Error, fmt::Display};
 
 use schema_discovery::SchemaDiscoverable;
 use serde_json_schema::Schema;
-
-pub enum SchemaRegistryIngestionError {
-    NoInternalIdentifier,
-    SchemaAlreadyExistsInRegistry,
-}
-
-pub enum SchemaRegistryDiscoveryError {
-    EncounteredDuplicateSchema,
-}
 
 #[derive(Default)]
 pub struct SchemaRegistry {
@@ -19,6 +10,10 @@ pub struct SchemaRegistry {
 }
 
 impl SchemaRegistry {
+    pub fn new() -> Self {
+        SchemaRegistry::default()
+    }
+
     pub fn add_internally_identified_schema(
         mut self,
         schema: Schema,
@@ -67,7 +62,52 @@ impl SchemaRegistry {
         Ok(self)
     }
 
+    pub fn get(&self, id: &str) -> Option<&Schema> {
+        self.schemas.get(id)
+    }
+
     fn schema_exists(&self, id: &str) -> bool {
         self.schemas.contains_key(id) || self.discovered_schemas.contains_key(id)
+    }
+}
+
+#[derive(Debug)]
+pub enum SchemaRegistryIngestionError {
+    NoInternalIdentifier,
+    SchemaAlreadyExistsInRegistry,
+}
+
+impl Error for SchemaRegistryIngestionError {}
+
+impl Display for SchemaRegistryIngestionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::NoInternalIdentifier => {
+                write!(f, "Attempted to register an internally identified schema, but it didn't have an internal id.")
+            }
+            Self::SchemaAlreadyExistsInRegistry => {
+                write!(
+                    f,
+                    "Attempted to register a schema, but there is an existing schema with that id."
+                )
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum SchemaRegistryDiscoveryError {
+    EncounteredDuplicateSchema,
+}
+
+impl Error for SchemaRegistryDiscoveryError {}
+
+impl Display for SchemaRegistryDiscoveryError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::EncounteredDuplicateSchema => {
+                write!(f, "Attempted to discover identifyable sub schemas from schemas in the repository, but while doing so encountered a duplicate.")
+            }
+        }
     }
 }
