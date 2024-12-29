5e77f4acea88b080b6506bee90e52546c80649e3
reef <public@ralph.tech>
2024-12-29 16:56:28 +0100

    added snake_case conversion for field names and optional for non-required fields

--- workspace/generators/rust_model_generator/src/lib.rs
+++ workspace/generators/rust_model_generator/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::error::Error;
 use std::fmt::Display;
 use std::iter;
@@ -14,28 +14,49 @@ use serde_json_schema::StringOrStringArray;
 // TODO external schemas
 // TODO rustify fieldnames
 // TODO validation>?
-pub fn generate(schema: Schema) -> Result<String> {
+pub fn generate(root_schema: Schema) -> Result<String> {
     let type_mapping = TypeMapping::with_basic_types();
-    let name = schema
+    let name = root_schema
         .title
         .ok_or(GeneratorError::RootSchemaIsMissingATitle)?;
     let name = Ident::new(&name, Span::call_site());
 
-    let mut fields: Vec<(&String, &Schema)> = schema.properties.inner_iter().collect();
+    let mut fields: Vec<(&String, &Schema)> = root_schema.properties.inner_iter().collect();
     fields.sort_by(|a, b| String::cmp(a.0, b.0));
     let fields = fields
         .into_iter()
-        .map(|(field_name, schema)| -> Result<TokenStream> {
+        .map(|(original_field_name, schema)| -> Result<TokenStream> {
             if schema.reference.is_some() {
                 todo!()
             }
 
             let json_type = match schema.schema_type.clone().ok_or(
-                GeneratorError::PropertyMissingTypeForField(field_name.to_owned()),
+                GeneratorError::PropertyMissingTypeForField(original_field_name.to_owned()),
             )? {
                 StringOrStringArray::String(value) => value,
                 StringOrStringArray::Array(_) => todo!(),
             };
+
+            let first_letter = original_field_name
+                .chars()
+                .next()
+                .unwrap_or_default()
+                .to_lowercase()
+                .next()
+                .unwrap();
+
+            let mut rest = original_field_name
+                .chars()
+                .skip(1)
+                .flat_map(|letter| match letter.is_uppercase() {
+                    true => vec!['_', letter.to_lowercase().next().unwrap()],
+                    false => vec![letter],
+                })
+                .collect::<VecDeque<char>>();
+
+            rest.push_front(first_letter);
+
+            let field_name = rest.iter().collect::<String>();
 
             let field_type =
                 match json_type.as_str() {
@@ -46,8 +67,16 @@ pub fn generate(schema: Schema) -> Result<String> {
                     )?,
                 };
 
-            let field_name = Ident::new(field_name, name.span());
             let field_type = Ident::new(field_type, name.span());
+            let field_type = root_schema
+                .required
+                .inner_iter()
+                .find(|r| original_field_name == *r)
+                .map(|_| quote! {#field_type})
+                .unwrap_or(quote! {Option<#field_type>});
+
+            let field_name = Ident::new(&field_name, name.span());
+
             let docs = schema.description.iter().map(|d| quote! {#[doc = #d]});
 
             let token_stream = quote! {
@@ -60,9 +89,9 @@ pub fn generate(schema: Schema) -> Result<String> {
         .collect::<Result<Vec<TokenStream>>>()?;
 
     let docs = iter::empty()
-        .chain(schema.dollar_id.iter())
-        .chain(schema.id.iter())
-        .chain(schema.description.iter())
+        .chain(root_schema.dollar_id.iter())
+        .chain(root_schema.id.iter())
+        .chain(root_schema.description.iter())
         .map(|s| s.to_owned())
         .map(|s| quote! {#[doc = #s]});
 
@@ -135,6 +164,12 @@ impl<'a, K, V> InnerIterator<'a, (&'a K, &'a V)> for Option<HashMap<K, V>> {
     }
 }
 
+impl<'a, V> InnerIterator<'a, &'a V> for Option<Vec<V>> {
+    fn inner_iter(&'a self) -> impl Iterator<Item = &'a V> {
+        self.iter().flatten()
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -168,19 +203,81 @@ mod tests {
         let schema: Schema = serde_json::from_str(json_string).unwrap();
         let result = generate(schema).unwrap();
 
-        let file_contents = r##"
+        let file_contents = quote! {
             ///https://example.com/person.schema.json
             struct Person {
                 ///Age in years which must be equal to or greater than zero.
-                age: i64,
+                age: Option<i64>,
                 ///The person's first name.
-                firstName: String,
+                first_name: Option<String>,
                 ///The person's last name.
-                lastName: String,
+                last_name: Option<String>,
             }
-        "##;
+        };
 
-        let syntax_tree = syn::parse_str(file_contents).unwrap();
+        let syntax_tree = syn::parse2(file_contents).unwrap();
+        let expected_result = prettyplease::unparse(&syntax_tree);
+
+        assert_eq!(result, expected_result);
+    }
+
+    /// https://json-schema.org/learn/json-schema-examples#address
+    #[test]
+    fn address_example() {
+        let json_string = r##"{
+                "$id": "https://example.com/address.schema.json",
+                "$schema": "https://json-schema.org/draft/2020-12/schema",
+                "title": "Address",
+                "description": "An address similar to http://microformats.org/wiki/h-card",
+                "type": "object",
+                "properties": {
+                    "postOfficeBox": {
+                        "type": "string"
+                    },
+                    "extendedAddress": {
+                        "type": "string"
+                    },
+                    "streetAddress": {
+                        "type": "string"
+                    },
+                    "locality": {
+                        "type": "string"
+                    },
+                    "region": {
+                        "type": "string"
+                    },
+                    "postalCode": {
+                        "type": "string"
+                    },
+                    "countryName": {
+                        "type": "string"
+                    }
+                },
+                "required": [ "locality", "region", "countryName" ],
+                "dependentRequired": {
+                    "postOfficeBox": [ "streetAddress" ],
+                    "extendedAddress": [ "streetAddress" ]
+                }
+            }"##;
+
+        let schema: Schema = serde_json::from_str(json_string).unwrap();
+        let result = generate(schema).unwrap();
+
+        let file_contents = quote! {
+            ///https://example.com/address.schema.json
+            ///An address similar to http://microformats.org/wiki/h-card
+            struct Address {
+                country_name: String,
+                extended_address: Option<String>,
+                locality: String,
+                post_office_box: Option<String>,
+                postal_code: Option<String>,
+                region: String,
+                street_address: Option<String>,
+            }
+        };
+
+        let syntax_tree = syn::parse2(file_contents).unwrap();
         let expected_result = prettyplease::unparse(&syntax_tree);
 
         assert_eq!(result, expected_result);
